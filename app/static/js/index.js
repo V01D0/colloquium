99e51331ec58dbb36e6605d867093b4d1320b8c8
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-08-08 11:54:24 +0530

    refactored index.js

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,7 +1,9 @@
 "use strict";
 const usernames = new Array();
+const Blogs = new Array();
 
 const expandNav = function () {
+  // Navigation
   const overlay = document.getElementById("overlay");
   const navBtn = document.getElementById("navBtn");
   const navList = document.querySelector("ul.nav-small");
@@ -19,6 +21,7 @@ const expandNav = function () {
 };
 
 const hideNav = function () {
+  // Navigation
   const overlay = document.getElementById("overlay");
   overlay.style.display = "none";
   navBtn.classList.add("fa-bars");
@@ -26,9 +29,11 @@ const hideNav = function () {
 };
 
 const disableButton = function () {
-  document.querySelector(".btn__submit").style.background = "#c7c7c7";
-  document.querySelector(".btn__submit").style.color = "black";
-  document.querySelector(".btn__submit").setAttribute("disabled", "");
+  // Function that disables a button and adds styles to it
+  const submit = document.querySelector(".btn__submit");
+  submit.style.background = "#c7c7c7";
+  submit.style.color = "black";
+  submit.setAttribute("disabled", "");
 };
 
 const loggedOut = function () {
@@ -36,13 +41,16 @@ const loggedOut = function () {
 };
 
 const enableButton = function () {
-  document.querySelector(".btn__submit").removeAttribute("disabled");
-  document.querySelector(".btn__submit").style.background = "#0057ff";
-  document.querySelector(".btn__submit").style.color = "white";
+  // Function that enables a button and adds styles to it
+  const submit = document.querySelector(".btn__submit");
+  submit.removeAttribute("disabled");
+  submit.style.background = "#0057ff";
+  submit.style.color = "white";
 };
 
 const checkUsernameInDB = function (username) {
-  const req = fetch(`/api/v1/checkuser?username=${username}`)
+  // Function to check whether username is available
+  const req = fetch(`/api/v1/user/check?username=${username}`)
     .then((response) => response.json())
     .then((data) => {
       if (data["auth"] === false) loggedOut();
@@ -56,6 +64,7 @@ const checkUsernameInDB = function (username) {
 };
 
 const validateSignup = function () {
+  // Function to validate Sign-Up page
   const inputs = Array.from(document.querySelectorAll("input"));
   const isValid = inputs.every((element) => element.checkValidity());
   const password = document.getElementById("password").value;
@@ -68,8 +77,8 @@ const validateSignup = function () {
 };
 
 const checkEmpty = function (element) {
+  // Check if this element's value is empty
   const isEmpty = element.value.trim() !== "" ? false : true;
-  console.log(isEmpty);
   if (isEmpty) {
     disableButton();
     element.setCustomValidity("This field can't be empty");
@@ -77,61 +86,136 @@ const checkEmpty = function (element) {
 };
 
 const validateBase = function () {
+  // Validate all inputs with class "required"
   const inputs = Array.from(document.querySelectorAll(".required"));
   const isValid = inputs.every((input) => input.checkValidity());
-  console.log(isValid);
   if (isValid) enableButton();
   else disableButton();
 };
 
-const generateUserString = function (element, checked = false, last = false) {
-  const userString = `<label class="user">${element}<input class="user__checkbox" type="checkbox" ${
+const generateUserString = function (
+  element,
+  checked = false,
+  last = false,
+  type = "",
+  desc = ""
+) {
+  let userString = `<label class="user">${element}<input class="user__checkbox" type="checkbox" ${
     checked ? `checked` : ``
-  } id="" onclick="checkUser(this)" name="" value="${element}"></label> ${
-    last ? "" : `<hr>`
-  }`;
+  } id="" onclick="checkUser(this,${type})" name="" value="${element}"></label>`;
+  if (type === "Blogs") {
+    userString += `<p class="blog__description">${desc}</p>`;
+  }
+  userString += `${last ? "" : `<hr>`}`;
+  // LEAVE CLASS NAMES ALONE, THEY CAN BE REUSED
+  // type IS ONLY REQUIRED FOR checkUser()
   return userString;
 };
 
 const getLikeUsers = function (friend) {
+  // Function to get usernames similar to friend%
   const autocompleteLabel = document.getElementById("autoComplete-label");
   const addedUsers = document.querySelector(".users__added");
-  // req.open("GET", `/api/v1/blogusercheck?friend=${friend}`);
+  if (document.querySelector(".user__list") === null) {
+    const users = document.createElement("div");
+    users.classList.add("user__list");
+    autocompleteLabel.insertAdjacentElement("afterend", users);
+    users.insertAdjacentElement("afterend", addedUsers);
+  }
+  const users = document.querySelector(".user__list");
   if (friend.trim() !== "") {
-    const friends = fetch(`/api/v1/blogusercheck?friend=${friend}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const usersLike = data["users"];
-        if (document.querySelector(".user__list") === null) {
-          const users = document.createElement("div");
-          users.classList.add("user__list");
-          autocompleteLabel.insertAdjacentElement("afterend", users);
-          users.insertAdjacentElement("afterend", addedUsers);
-        }
-        if (usersLike.length === 0) {
-          const users = document.querySelector(".user__list");
-          users.innerHTML = `NO MATCHES FOUND`;
-          users.style.fontWeight = 300;
-          users.style.border = "none";
-        }
-        usersLike.forEach((element, index, arr) => {
-          const users = document.querySelector(".user__list");
-          if (users != null) {
+    if (friend.length < 3) {
+      users.innerHTML = `NEED AT LEAST 3 CHARACTERS`;
+      users.style.fontWeight = 300;
+      users.style.border = "none";
+    } else {
+      const friends = fetch(`/api/v1/users/like?friend=${friend}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const usersLike = data["users"];
+          if (usersLike.length === 0) {
+            users.innerHTML = `NO MATCHES FOUND`;
+            users.style.fontWeight = 300;
+            users.style.border = "none";
+          }
+          usersLike.forEach((element, index, arr) => {
             users.innerHTML = "";
             users.style.fontWeight = "inherit";
-          }
-          if (index === arr.length - 1) {
-            users.innerHTML += usernames.includes(element)
-              ? generateUserString(element, true, true)
-              : generateUserString(element, false, true);
+            if (index === arr.length - 1) {
+              users.innerHTML += usernames.includes(element)
+                ? generateUserString(element, true, true, "usernames")
+                : generateUserString(element, false, true, "usernames");
+            } else {
+              const res = usernames.includes(element) ? true : false;
+              users.innerHTML += generateUserString(
+                element,
+                res,
+                false,
+                "usernames"
+              );
+            }
+          });
+        });
+      if (users != null) {
+        users.style.border = "1px solid black";
+      }
+    }
+  } else if (document.querySelector(".user__list") != null) {
+    document.querySelector(".user__list").style.border = "none";
+    document.querySelector(".user__list").innerHTML = "";
+  }
+};
+
+const getLikeBlogs = function (blogName) {
+  // Function to get blogs similar to %blogName%
+  const blogsLabel = document.querySelector(".blogs__label");
+  const addedBlogs = document.querySelector(".users__added");
+  if (document.querySelector(".user__list") === null) {
+    const blogs = document.createElement("div");
+    blogs.classList.add("user__list");
+    blogsLabel.insertAdjacentElement("afterend", blogs);
+    blogs.insertAdjacentElement("afterend", addedBlogs);
+  }
+  const blogs = document.querySelector(".user__list");
+  if (blogName.trim() !== "") {
+    if (blogName.length < 3) {
+      blogs.innerHTML = `NEED AT LEAST 3 CHARACTERS`;
+      blogs.style.fontWeight = 300;
+      blogs.style.border = "none";
+    } else {
+      const blog = fetch(`/api/v1/blogs/like?name=${blogName}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const blogsLike = data["blogs"];
+          if (Object.keys(blogsLike).length === 0) {
+            blogs.innerHTML = `NO MATCHES FOUND`;
+            blogs.style.fontWeight = 300;
+            blogs.style.border = "none";
           } else {
-            const res = usernames.includes(element) ? true : false;
-            users.innerHTML += generateUserString(element, res, false);
+            blogs.innerHTML = "";
+            Object.entries(blogsLike).forEach(([key, value], index) => {
+              blogs.style.marginBottom = "1rem";
+              blogs.style.fontWeight = "inherit";
+              if (index === Object.keys(blogsLike).length - 1) {
+                blogs.innerHTML += Blogs.includes(key)
+                  ? generateUserString(key, true, true, "Blogs", value)
+                  : generateUserString(key, false, true, "Blogs", value);
+              } else {
+                const res = Blogs.includes(key) ? true : false;
+                blogs.innerHTML += generateUserString(
+                  key,
+                  res,
+                  false,
+                  "Blogs",
+                  value
+                );
+              }
+            });
           }
         });
-      });
-    if (document.querySelector(".user__list") != null) {
-      document.querySelector(".user__list").style.border = "1px solid black";
+      if (blogs != null) {
+        blogs.style.border = "1px solid black";
+      }
     }
   } else if (document.querySelector(".user__list") != null) {
     document.querySelector(".user__list").style.border = "none";
@@ -140,8 +224,9 @@ const getLikeUsers = function (friend) {
 };
 
 const checkBlog = function (blogName) {
+  // Function to check if blogName is available
   if (blogName.length >= 3) {
-    const req = fetch(`/api/v1/blogcheck?blog=${blogName}`)
+    const req = fetch(`/api/v1/blog/check?blog=${blogName}`)
       .then((response) => response.json())
       .then((data) => {
         if (data["auth"] === false) loggedOut();
@@ -156,15 +241,16 @@ const checkBlog = function (blogName) {
   }
 };
 
-const checkUser = function (checkbox) {
+const checkUser = function (checkbox, type) {
+  // Function to handle checkbox related actions
   const addedUsers = document.querySelector(".users__added");
-  if (checkbox.checked) usernames.push(checkbox.value);
+  if (checkbox.checked) type.push(checkbox.value);
   else {
-    const index = usernames.indexOf(checkbox.value);
-    usernames.splice(index, 1);
+    const index = type.indexOf(checkbox.value);
+    type.splice(index, 1);
   }
   addedUsers.value = "";
-  usernames.forEach((element, index, arr) => {
+  type.forEach((element, index, arr) => {
     if (arr.length !== 1) {
       addedUsers.value += index !== arr.length - 1 ? `${element}|` : element;
     } else {
