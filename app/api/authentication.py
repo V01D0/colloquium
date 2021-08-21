fa051a20c1bd092df6a4db9267e736f17ef06c9a
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-08-21 23:50:09 +0530

    created resource for refreshing token

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,8 +1,15 @@
 from flask_restful import Resource, reqparse
 from ..models import Users
 from . import api
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity,
+    get_jwt,
+)
 from flask import jsonify
+from datetime import datetime, timedelta, timezone
 
 
 class Username(Resource):
@@ -25,7 +32,7 @@ class Username(Resource):
         return {"status": False}
 
 
-api.add_resource(Username, "/checkuser")
+api.add_resource(Username, "/user/check")
 
 
 class Login(Resource):
@@ -50,12 +57,30 @@ class Login(Resource):
         user = Users.query.filter_by(username=args.username).first()
         result = {"status": False}
         if user is not None and user.verify_password(args.password):
+            user_id = user.id
             result["status"] = True
-            result["access_token"] = create_access_token(
-                identity=args.username, fresh=True
-            )
-            result["refresh_token"] = create_refresh_token(identity=args.username)
+            result["access_token"] = create_access_token(identity=user_id, fresh=True)
+            result["refresh_token"] = create_refresh_token(identity=user_id)
         return result
 
 
 api.add_resource(Login, "/auth/login")
+
+class Refresh(Resource):
+    """Refresh JWT token"""
+
+    @jwt_required(refresh=True)
+    def post(self):
+        """on POST"""
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        identity = get_jwt_identity()
+        result = {}
+        if target_timestamp > exp_timestamp:
+            result["refresh_token"] = create_refresh_token(identity=identity)
+        result["access_token"] = create_access_token(identity=identity, fresh=True)
+        return result
+
+
+api.add_resource(Refresh, "/auth/refresh")
