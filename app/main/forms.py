05c9bd7cd8cee0cbb48ae18b41ab37e1ba7b30d8
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-08-24 22:09:20 +0530

    created form for post submissions

--- app/main/forms.py
+++ app/main/forms.py
@@ -25,17 +25,44 @@ class CreateBlog(FlaskForm):
             ),
         ],
     )
-    # subject = StringField('Subject/Title', validators=[DataRequired()])
+    heading = StringField(
+        "Heading/Title",
+        validators=[DataRequired("Please give your blog a title/heading")],
+    )
     description = TextAreaField(
         "Describe your blog",
         validators=[DataRequired("Please fill in the description of your blog")],
     )
-    invite_list = StringField("Invite a friend or two", [Optional()])
+    invite_list = StringField("Invite a friend or two", validators=[Optional()])
     added_users = HiddenField()
-    # remember_me = BooleanField('Remember me')
     posters = RadioField(
         "Decide who can post on this blog",
         validators=[DataRequired("Please choose who can post on your blog")],
         choices=[("0", "Anyone"), ("1", "Only me"), ("2", "Custom")],
     )
     submit = SubmitField("Create my blog!")
+
+
+class PostSubmit(FlaskForm):
+    """Form to create/submit posts"""
+
+    blogs = StringField(
+        "Select the blogs you'd like to share this to",
+        validators=[DataRequired("Please select at least one community to post to")],
+    )
+    added_blogs = HiddenField()
+    title = StringField(
+        "Your post title",
+        validators=[
+            length(min=1, max=100, message="Can't have an empty title!"),
+            DataRequired("Can't have an empty title!"),
+        ],
+    )
+    body = TextAreaField(
+        "Your post body",
+        validators=[
+            length(min=1, message="Can't have an empty body"),
+            DataRequired("Can't have an empty body"),
+        ],
+    )
+    submit = SubmitField("Submit!")
